257c24840a5bafa7e482cc9c9835b3e42a606f50
huangkangkk-ship-it <huangkangkk@gmail.com>
2026-08-17 14:40:28 +0800

    fix: globally exclude conflicting legacy Kotlin jdk stdlib modules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.huangkang.gtneo2tint"
         minSdk = 23
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     compileOptions {
@@ -20,11 +20,13 @@ android {
     }
 }
 
+configurations.all {
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+}
+
 dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.otaliastudios:cameraview:2.7.2") {
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
-        exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
-    }
+    implementation("com.otaliastudios:cameraview:2.7.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.22")
 }
